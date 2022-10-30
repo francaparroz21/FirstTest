c16a4f63f16693682311b5fcbf052d75ec6889dd
francaparroz21 <francaparroz21@gmail.com>
2022-10-30 17:17:31 -0300

    we created twoSumManyValues functions to see how is the process

--- src/main/java/pil/First.java
+++ src/main/java/pil/First.java
@@ -2,17 +2,13 @@ package pil;
 
 public class First {
     public static int[] twoSum(int[] arr, int target){
-        int[] result = new int[]{0,0};
         if (arr.length >= 2){
             for (int i = 0; i < arr.length; i++) {
                 for (int j = 0; j < arr.length ; j++) {
-                    if (i !=j && arr[i] + arr[j] == target){
-                        result[i] = i;
-                        result[j] = j;
-                    }
+                    if (i !=j && arr[i] + arr[j] == target)return new int[] {i,j};
                 }
             }
         }
-        return result;
+        return new int[]{0,0};
     }
 }
--- src/test/java/pil/FirstTest.java
+++ src/test/java/pil/FirstTest.java
@@ -26,6 +26,17 @@ public class FirstTest {
         assertEquals(expected[1], result[1]);
     }
 
+    //The function TwoSum returns the first values founded.
+    @Test
+    public void twoSumManyValues(){
+        int[] arr = {1,3,10,9,13,20,3};
+        int target = 23;
+        int[] result = twoSum(arr,target);
+        int[] expected = {1,5};
+        assertEquals(expected[0], result[0]);
+        assertEquals(expected[1], result[1]);
+    }
+
     @Test
     public void twoSumSingleValue(){
         int[] arr = {5};
