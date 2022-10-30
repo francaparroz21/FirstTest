1cd3cc9f9b74c8622fb04af61ef2cee6a202db32
francaparroz21 <francaparroz21@gmail.com>
2022-10-30 17:04:20 -0300

    First Test Repository

---
+++ src/test/java/pil/FirstTest.java
@@ -0,0 +1,38 @@
+package pil;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static pil.First.*;
+
+public class FirstTest {
+
+    @Test
+    public void twoSumFound(){
+        int[] arr = {2,5,1};
+        int target = 7;
+        int[] result = twoSum(arr, target);
+        int[] expected = {0, 1};
+        assertEquals(expected[0], result[0]);
+        assertEquals(expected[1], result[1]);
+    }
+
+    @Test
+    public void twoSumNotFound(){
+        int[] arr = {1,4,9,10};
+        int target = 6;
+        int[] result = twoSum(arr,target);
+        int[] expected = {0,0};
+        assertEquals(expected[0], result[0]);
+        assertEquals(expected[1], result[1]);
+    }
+
+    @Test
+    public void twoSumSingleValue(){
+        int[] arr = {5};
+        int target = 5;
+        int[] result = twoSum(arr,target);
+        int[] expected = {0,0};
+        assertEquals(expected[0], result[0]);
+        assertEquals(expected[1], result[1]);
+    }
+}
